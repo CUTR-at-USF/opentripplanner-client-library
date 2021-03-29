c391b0af24655994867c3ded0aedb44bb6bbc62d
BumbleFlash <sudarshan97@gmail.com>
2021-03-29 16:22:38 -0400

    chore: added option to pass in host, port and parameters to reduce developer friction!

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import edu.usf.cutr.otp.planner.api.PlannerApi
 import edu.usf.cutr.otp.planner.model.Planner
+import edu.usf.cutr.otp.planner.model.RequestParameters
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.Main
 import kotlin.coroutines.CoroutineContext
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
 
     private lateinit var api: PlannerApi
     private lateinit var job: Job
-
+    private lateinit var requestParameters: RequestParameters
     private val TAG = "MainActivity"
 
     override val coroutineContext: CoroutineContext
@@ -25,7 +26,10 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
         setContentView(R.layout.activity_main)
 
         job = Job()
-        api = PlannerApi()
+        requestParameters = RequestParameters(
+            fromPlace = latLong(41.84712, -87.64678),
+            toPlace = latLong(41.84584, -87.65214))
+        api = PlannerApi("10.0.2.2", 8080, requestParameters)
         api.getPlannerResource(
             success = { launch (Main) { logData(it) } },
             failure = ::handleError
@@ -39,5 +43,9 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
     private fun handleError(ex: Throwable?) {
         ex?.message?.let { Log.d(TAG, it) }
         ex?.printStackTrace()
+    }
+
+    private fun latLong(lat: Double, long: Double): String {
+        return "$lat,$long"
     }
 }
--- shared/src/commonMain/kotlin/edu/usf/cutr/otp/planner/api/PlannerApi.kt
+++ shared/src/commonMain/kotlin/edu/usf/cutr/otp/planner/api/PlannerApi.kt
@@ -2,22 +2,32 @@ package edu.usf.cutr.otp.planner.api
 
 import edu.usf.cutr.otp.ApplicationDispatcher
 import edu.usf.cutr.otp.planner.model.Planner
+import edu.usf.cutr.otp.planner.model.RequestParameters
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import io.ktor.client.*
 import io.ktor.client.request.*
+import io.ktor.http.*
 import kotlinx.serialization.json.Json
 
-class PlannerApi() {
-    private val testUrl = "http://10.0.2.2:8080/otp/routers/default/plan?fromPlace=41.84712%2c-87.64678&toPlace=41.84584%2c-87.65214"
+class PlannerApi(private val host: String,
+                 private val port: Int,
+                 private val requestParameters: RequestParameters) {
+
+    //private val testUrl = "http://10.0.2.2:8080/otp/routers/default/plan?fromPlace=41.84712%2c-87.64678&toPlace=41.84584%2c-87.65214"
 
     fun getPlannerResource(
         success: (Planner) -> Unit, failure: (Throwable?) -> Unit) {
 
         GlobalScope.launch(ApplicationDispatcher) {
             try {
-                val url = testUrl
-
+                val parameters = buildParameters(requestParameters)
+                val url = URLBuilder(
+                    protocol = URLProtocol.HTTP,
+                    host = host,
+                    port = port,
+                    parameters = parameters,
+                ).path("otp", "routers", "default", "plan").buildString()
                 val json = HttpClient().get<String>(url)
 
                 Json.decodeFromString(Planner.serializer(), json).also(success)
@@ -25,6 +35,59 @@ class PlannerApi() {
                 failure(ex)
             }
         }
+    }
+
+    private fun buildParameters(requestParameters: RequestParameters): ParametersBuilder {
+        val parameters = ParametersBuilder()
+        if (requestParameters.fromPlace != null) {
+            parameters.append("fromPlace", requestParameters.fromPlace.toString())
         }
+        if (requestParameters.toPlace != null) {
+            parameters.append("toPlace", requestParameters.toPlace.toString())
+        }
+        if (requestParameters.arriveBy != null) {
+            parameters.append("arriveBy", requestParameters.arriveBy.toString())
+        }
+        if (requestParameters.alightSlack != null) {
+            parameters.append("alightSlack", requestParameters.alightSlack.toString())
+        }
+        if (requestParameters.traverseModes != null) {
+            parameters.append("traverseModes", requestParameters.traverseModes.toString())
+        }
+        if (requestParameters.optimizeType != null) {
+            parameters.append("optimizeType", requestParameters.optimizeType.toString())
+        }
+        if (requestParameters.intermediatePlaces != null) {
+            parameters.append("intermediatePlaces", requestParameters.intermediatePlaces.toString())
+        }
+        if (requestParameters.maxWalkDistance != null) {
+            parameters.append("maxWalkDistance", requestParameters.maxWalkDistance.toString())
+        }
+        if (requestParameters.triangleTimeFactor != null) {
+            parameters.append("triangleTimeFactor", requestParameters.triangleTimeFactor.toString())
+        }
+        if (requestParameters.triangleSlopeFactor != null) {
+            parameters.append("triangleSlopeFactor", requestParameters.triangleSlopeFactor.toString())
+        }
+        if (requestParameters.triangleSafetyFactor != null) {
+            parameters.append("triangleSafetyFactor", requestParameters.triangleSafetyFactor.toString())
+        }
+        if (requestParameters.wheelchair != null) {
+            parameters.append("wheelchair", requestParameters.wheelchair.toString())
+        }
+        if (requestParameters.date != null) {
+            parameters.append("date", requestParameters.date.toString())
+        }
+        if (requestParameters.numItineraries != null) {
+            parameters.append("numItineraries", requestParameters.numItineraries.toString())
+        }
+        if (requestParameters.preferredRoutes != null) {
+            parameters.append("preferredRoutes", requestParameters.preferredRoutes.toString())
+        }
+        if (requestParameters.unpreferredRoutes != null) {
+            parameters.append("unpreferredRoutes", requestParameters.unpreferredRoutes.toString())
+        }
+        return parameters
+    }
 
 }
