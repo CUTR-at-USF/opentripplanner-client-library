0c9b7bc2639df3867986050243ba4bf2b9fda670
BumbleFlash <sudarshan97@gmail.com>
2021-03-31 15:51:23 -0400

    build: changed app id and name

--- app/src/main/java/edu/usf/cutr/otp/demo/MainActivity.kt
+++ app/src/main/java/edu/usf/cutr/otp/demo/MainActivity.kt
@@ -13,7 +13,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  */
-package com.example.myapplication
+package edu.usf.cutr.otp.demo
 
 import android.os.Bundle
 import android.util.Log
