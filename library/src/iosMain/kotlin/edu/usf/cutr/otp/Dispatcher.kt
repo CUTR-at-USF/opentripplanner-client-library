7cfaa8f8fbe42641471827cf1f20704ff1044494
Sean Barbeau <sjbarbeau@gmail.com>
2021-04-05 18:48:45 -0400

    chore: Cleanup iOS code

--- library/src/iosMain/kotlin/edu/usf/cutr/otp/Dispatcher.kt
+++ library/src/iosMain/kotlin/edu/usf/cutr/otp/Dispatcher.kt
@@ -4,15 +4,9 @@ import kotlin.coroutines.*
 import kotlinx.coroutines.*
 import platform.darwin.*
 
-// 1
-internal actual val ApplicationDispatcher: CoroutineDispatcher =
-    NsQueueDispatcher(dispatch_get_main_queue())
+internal actual val ApplicationDispatcher: CoroutineDispatcher = NsQueueDispatcher(dispatch_get_main_queue())
 
-// 2
-internal class NsQueueDispatcher(
-    private val dispatchQueue: dispatch_queue_t
-) : CoroutineDispatcher() {
-
+internal class NsQueueDispatcher(private val dispatchQueue: dispatch_queue_t) : CoroutineDispatcher() {
     override fun dispatch(context: CoroutineContext, block: Runnable) {
         dispatch_async(dispatchQueue) {
             block.run()
