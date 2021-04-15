40cf14a764a821b178623042703ee38d0d2b7fb8
Sudarshan <sudarshan97@gmail.com>
2021-04-15 09:17:45 -0400

    fix: fix test JSON URLs (#19)

--- library/src/androidTest/kotlin/edu/usf/cutr/otp/chicago/ServerInfoApiUnitTest.kt
+++ library/src/androidTest/kotlin/edu/usf/cutr/otp/chicago/ServerInfoApiUnitTest.kt
@@ -10,7 +10,7 @@ class ServerInfoApiUnitTest {
     @Test
     fun testServerInfoApi() {
         val serverInfoApi =
-            ServerInfoApi("https://raw.githubusercontent.com/CUTR-at-USF/opentripplanner-client-library/tests/library/src/jvmTest/resources/chicago/server_info.json")
+            ServerInfoApi("https://raw.githubusercontent.com/CUTR-at-USF/opentripplanner-client-library/main/library/src/jvmTest/resources/chicago/server_info.json")
 
         var serverInfo = ServerInfo()
         val latch = CountDownLatch(1)
